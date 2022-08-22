007736f88d55809e4c640f62331d42a2c6e2bde5
maenoWin-codes <cacortesl01@gmail.com>
2022-08-22 16:17:34 -0500

    Se crean las funciones para renderizar los objetos del mapa y el tamaño responsive del canvas

--- main.js
+++ main.js
@@ -1,8 +1,56 @@
+
 const canvas = document.getElementById('game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+window.addEventListener('load', resizeCanvas);
+window.addEventListener('resize', resizeCanvas);
 
-function startGame(){
-    game.fillRect(0, 0, 0, 5);
+function resizeCanvas(){
+    let canvasSize;
+    //Breakpoints para el canvas
+    if(window.innerWidth > 900){
+        canvasSize = window.innerHeight * 0.8;
+       
+    }
+    else if(window.innerWidth > 600 && window.innerWidth < 900){ 
+       
+        canvasSize = window.innerWidth * 0.6;  
+      
+    }
+    else{
+        canvasSize = window.innerWidth * 0.7;
+    
+    }
+    //Se establece las medidas del canvas
+    canvas.height = canvasSize;
+    canvas.width = canvasSize;
+    return  startGame(canvasSize);
+}
+
+function startGame(canvasSize){
+    
+    let elementSize;
+    let map = maps[2];
+    const mapRow = map.trim().split('\n'); 
+    map = mapRow.map( row => row.trim().split(''));
+    console.log(map);
+    
+    elementSize = canvasSize/10;
+   
+    game.font = elementSize-5+'px Vernada';
+    game.textAlign = 'end';
+
+    console.log(map[0][2]);
+
+    for(let i = 1; i <= 10; i++){
+        for (let j = 1; j <= 10; j++) {
+           
+            game.fillText(emojis[map[i-1][j-1]], (elementSize)*j, (elementSize)*(i-0.18));
+            
+        }
+        
+    }
+    /*  game.fillStyle = 'Gold'; */
+    
+   /*  game.fillRect(0, 0, 50, 50); */
 }
