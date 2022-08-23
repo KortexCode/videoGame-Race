1a6afd2912efd5a6dbabfcdd9bc92c7b44d08a9f
maenoWin-codes <cacortesl01@gmail.com>
2022-08-23 16:28:06 -0500

    Modificaciones al JS, se configura movimiento al persona sin salirse del canvas y se generar movimiento sin duplicar al personaje PLAYER.

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 
 //ELEMENTS
+/*Posición que separa a cada elemento*/
+let elementSize;
+let canvasSize;
 /*Canvas*/
 const canvas = document.getElementById('game');
 const game = canvas.getContext('2d');
+/*Player*/
+const player = {x: null,
+                y: null,}
 /*Buttons*/
 const btnUp = document.getElementById('up');
 const btnDown = document.getElementById('down');
 const btnLeft = document.getElementById('left');
 const btnRight = document.getElementById('right');
+
 
 //EVENTS
 /*Load and Reload */
@@ -22,7 +29,6 @@ btnLeft.addEventListener('click', moveLeft);
 btnRight.addEventListener('click', moveRight);
 
 function resizeCanvas(){
-    let canvasSize;
     //Breakpoints para el canvas
     if(window.innerWidth > 900){
         canvasSize = window.innerHeight * 0.8;   
@@ -36,26 +42,37 @@ function resizeCanvas(){
     //Se establece las medidas del canvas
     canvas.height = canvasSize;
     canvas.width = canvasSize;
-    return  startGame(canvasSize);
+    return  startGame();
 }
 
-function startGame(canvasSize){
-    
-    let elementSize;
+function startGame(){
+
     elementSize = canvasSize/10.1; //10 elementos por cada mapa
     game.font = elementSize-5+'px Vernada';
     game.textAlign = 'end';
     //Se acoondiciona el mapa a una matrix 10x10
-    let map = maps[0];
+    let map = maps[1];
     const mapRow = map.trim().split('\n'); 
     map = mapRow.map( row => row.trim().split(''));
+    //set renderizado
+    game.clearRect(0, 0, canvasSize, canvasSize);
     //Renderizado del mapa
     map.forEach((row, rowIndex) => {
         row.forEach((col, colIndex) => {
-            const posX = elementSize*(colIndex+1);
+            const posX = elementSize*(colIndex+1.15);
             const posY = elementSize*(rowIndex+0.88);
             game.fillText(emojis[col], posX, posY);
+            if(emojis[col]=='🚧'){
+                if(!player.x && !player.y){
+                    player.x = posX;
+                    player.y = posY;
+                    console.log('playerx', player.x);
+                    console.log('playery', player.y);
+                    
+                }
+            }
         });
+        game.fillText(emojis['PLAYER'], player.x, player.y);
     });
 
  /*    for(let i = 1; i <= 10; i++){
@@ -83,13 +100,34 @@ function moveByKey(event){
 
 function moveUp(){
     console.log('mover arriba');
+    if(!(player.y < elementSize)){
+        game.fillText(emojis['PLAYER'], player.x, player.y-=(elementSize));
+        startGame();
+    }
+   
 }
 function moveDown(){
     console.log('mover abajo');
+    
+    if(!(player.y > canvasSize-elementSize)){
+        game.fillText(emojis['PLAYER'], player.x, player.y+=(elementSize));
+        startGame();
+    }
 }
 function moveLeft(){
     console.log('mover izquierda')
+
+    if(!(player.x < elementSize*2)){
+        game.fillText(emojis['PLAYER'], player.x-=(elementSize), player.y);
+        console.log(player.x);
+        startGame();
+    }
 }
 function moveRight(){
     console.log('mover derecha')
+    
+    if(!(player.x > canvasSize)){
+        game.fillText(emojis['PLAYER'], player.x+=(elementSize), player.y);
+        startGame();
+    }
 }
